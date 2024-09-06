118a3b76c4a5be72426f0663c22415896888189d
Malaika Saba <malaikasaba3@gmail.com>
2024-09-06 12:06:58 +0500

    basics 2

---
+++ Arrays/Lecture4.js
@@ -0,0 +1,13 @@
+
+// ARRAYS IN JS
+// Arrays store same type of data/ collection od data 
+
+let marks = [ 45,67,89,99,90,78];
+console.log(marks);
+console.log(marks.length);
+marks[4] = 92;
+console.log(marks);
+
+// Arrays indices
+// arr[0], arr[1], arr[2] etc
+
