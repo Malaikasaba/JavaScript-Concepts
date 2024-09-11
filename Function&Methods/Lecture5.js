58572b589564d522563d97733e7d051a6a3d4edc
Malaika Saba <malaikasaba3@gmail.com>
2024-09-11 10:27:51 +0500

    Functions

---
+++ Function&Methods/Lecture5.js
@@ -0,0 +1,34 @@
+// Functions in JS
+// Functions are block of code that perform specific tasks/
+// can be invoked whenever needed.
+
+
+// Function Syntax
+/*
+function myFunction () {
+ block of code
+}
+myFunction();
+
+*/
+
+// Example
+function myFunction (){
+    console.log("My first function");
+    console.log("Hello World");
+    
+}
+myFunction (); 
+// OR
+function myFunction (msg){ // parameter --> input
+    console.log(msg);
+}
+myFunction ("I love JavaScript"); // argument 
+
+// Function to calculate sum of 2 numbers
+
+function sum (a, b){
+s = a + b;
+return
+}
+sum();
